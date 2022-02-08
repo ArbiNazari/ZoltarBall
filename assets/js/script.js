7c6adb6be79bda03527c1844d8928d2b6f3d64a5
Kyle Patrick Bader <kbader91@gmail.com>
2022-02-08 15:04:14 -0800

    display and search button updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,18 @@
-teamListEl = document.querySelector("#teams").children;
+teamListEl = document.querySelector("#teams");
 teamFormSearchEl = document.querySelector("#find-team");
+heroContainerEl = document.querySelector("#hero-section");
+searchBtnEl = document.querySelector(".btn-search");
 // console.log(teamListEl);
 
 
-function formSubmitHandler(event) {
-    // prevents refresh
-    event.preventDefault();
-
-    if (teamListEl) {
-        getStats();
-    }
-};
-teamFormSearchEl.addEventListener("submit", formSubmitHandler);
-
-
-// display stats 
 function displayStats() {
-
+// removes original hero banner 
+heroContainerEl.classList.remove("hero"); 
 }
 
 
 // GET STATS
-function getStats () {
+function getStats() {
     fetch("https://nfl-team-stats.p.rapidapi.com/v1/nfl-stats/teams/win-stats/2020", {
         "method": "GET",
         "headers": {
@@ -93,70 +84,9 @@ function getStats () {
     .catch(err => {
         console.error(err);
     });
+
+    displayStats();
 };
 
-
-
-// // RECEIVING stats 
-// function getReceptions() {
-//     fetch("https://nfl-team-stats.p.rapidapi.com/v1/nfl-stats/teams/receiving-stats/offense/2019", {
-//         "method": "GET",
-//         "headers": {
-//             "x-rapidapi-host": "nfl-team-stats.p.rapidapi.com",
-//             "x-rapidapi-key": "a8143de9d6msh6d1195065a85dbfp18ac06jsn532055b2fc85"
-//         }
-//     })
-//     .then(response => {
-//         console.log(response);
-//         return response.json();
-//     })
-//     .then(data => {
-//         console.log(data);
-//     })
-//     .catch(err => {
-//         console.error(err);
-//     });
-// };
-//
-
-// RUSHING stats 
-// function getRushing() {
-//     fetch("https://nfl-team-stats.p.rapidapi.com/v1/nfl-stats/teams/rushing-stats/defense/1999", {
-//         "method": "GET",
-//         "headers": {
-//             "x-rapidapi-host": "nfl-team-stats.p.rapidapi.com",
-//             "x-rapidapi-key": "a8143de9d6msh6d1195065a85dbfp18ac06jsn532055b2fc85"
-//         }
-//     })
-//     .then(response => {
-//         console.log(response);
-//         return response.json();
-//     })
-//     .then(data => {
-//         console.log(data);
-//     })
-//     .catch(err => {
-//         console.error(err);
-//     });
-// };
-
-// PASSING stats 
-// function getPassing() {
-//     fetch("https://nfl-team-stats.p.rapidapi.com/v1/nfl-stats/teams/passing-stats/offense/2021", {
-//         "method": "GET",
-//         "headers": {
-//             "x-rapidapi-host": "nfl-team-stats.p.rapidapi.com",
-//             "x-rapidapi-key": "a8143de9d6msh6d1195065a85dbfp18ac06jsn532055b2fc85"
-//         }
-//     })
-//     .then(response => {
-//         console.log(response);
-//         return response.json();
-//     })
-//     .then(data => {
-//         console.log(data);
-//     })
-//     .catch(err => {
-//         console.error(err);
-//     });
-// };
+// get stats button 
+searchBtnEl.addEventListener("click", getStats);
