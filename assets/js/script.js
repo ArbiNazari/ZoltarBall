46c914ffab3f2cd7e255dba0098325fdde8d55ad
Kyle Patrick Bader <kbader91@gmail.com>
2022-02-08 18:07:34 -0800

    arrays for data added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,84 @@ teamListEl = document.querySelector("#teams");
 teamFormSearchEl = document.querySelector("#find-team");
 heroContainerEl = document.querySelector("#hero-section");
 searchBtnEl = document.querySelector(".btn-search");
+var passFetchArr = [];
+var winFetchArr = [];
+var rushFetchArr = [];
+var recFetchArr = [];
 // console.log(teamListEl);
 
 
 function displayStats() {
 // removes original hero banner 
 heroContainerEl.classList.remove("hero"); 
-}
+heroContainerEl.classList.add("stats-team");
+
+
+var x = document.getElementById("teams");
+var y = x.options[x.selectedIndex].text;
+// console.log(value);
+
+var teamName = document.createElement("h2");
+teamName.innerHTML = y;
+heroContainerEl.append(teamName);
+
+var selectedTeam = document.getElementById("teams");
+	var winTeamName = selectedTeam.getAttribute('data-team');
+	var teamName = selectedTeam.val;
+	
+    console.log("stuff");
+    console.log(winFetchArr);
+
+	//Wins Array
+	$.each(winFetchArr, function() {
+		if(winTeamName == this.name) {
+
+			winRatePercentage = this.winRatePercentage;
+           
+		}
+	});
+	//Receiving Array
+	$.each(recFetchArr, function() {
+		if(teamName == this.name) {
+			yards = this.yards;
+		}
+	});
+    // Passing Array
+    $.each(passFetchArr, function() {
+		if(teamName == this.name) {
+			passYards = this.pass;
+            completions = this.completions;
+            touchdowns = this.touchdowns;
+		}
+	});
+     // Rushing Array
+     $.each(rushFetchArr, function() {
+		if(teamName == this.name) {
+			rushYards = this.rush;
+		}
+	});
+
+        var teamWins = document.createElement("h4");
+        teamWins.innerHTML = "Win Percentage: " + winRatePercentage;
+        heroContainerEl.append(teamWins);
+
+        var teamPassing = document.createElement("h4");
+        teamPassing.innerHTML = "Passing yards per game: " + passing + "<br />Completions: " + completions + "<br />Touchdowns: " + touchdowns;
+        heroContainerEl.append(teamPassing);
+
+        var teamRushing = document.createElement("h4");
+        teamRushing.innerHTML = "Rushing yards per game: " + rushYards;
+        heroContainerEl.append(teamRushing);
+
+        var teamReceptions = document.createElement("h4");
+        teamReceptions.innerHTML = "Receiving yards per game: " + yards;
+        heroContainerEl.append(teamReceptions);
+
+
+};
+
+// get stats button 
+searchBtnEl.addEventListener("click", getStats);
 
 
 // GET STATS
@@ -23,9 +94,13 @@ function getStats() {
     .then(response => {
         // console.log(response);
         return response.json();
+        
     })
     .then(data => {
+        
+        winFetchArr = $.parseJSON(data);
         console.log(data);
+        
     })
     .catch(err => {
         console.error(err);
@@ -43,6 +118,7 @@ function getStats() {
         return response.json();
     })
     .then(data => {
+        recFetchArr = data;
         console.log(data);
     })
     .catch(err => {
@@ -61,6 +137,7 @@ function getStats() {
         return response.json();
     })
     .then(data => {
+        rushFetchArr = data;
         console.log(data);
     })
     .catch(err => {
@@ -79,6 +156,7 @@ function getStats() {
         return response.json();
     })
     .then(data => {
+        passFetchArr = data;
         console.log(data);
     })
     .catch(err => {
@@ -88,5 +166,6 @@ function getStats() {
     displayStats();
 };
 
-// get stats button 
-searchBtnEl.addEventListener("click", getStats);
+
+
+
